7a9e52212098e28bac53c7e5b176da14f41d4ef4
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2024-04-27 01:48:56 -0400

    Updated Logo and logo text, checking for safari issues

--- app/components/navigation/Navbar.jsx
+++ app/components/navigation/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./nav.module.css";
 import MegaMenu from "./MegaMenu";
 import { Menu, ShoppingBag, Globe, Minimize2 } from "lucide-react";
@@ -8,6 +9,7 @@ import { Menu, ShoppingBag, Globe, Minimize2 } from "lucide-react";
 export default function Navbar() {
   //states
   const [megaMenuToggle, setMegaMenuToggle] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
 
   //util functions
   const toggleMenu = () => {
@@ -18,13 +20,23 @@ export default function Navbar() {
     <>
       <MegaMenu state={megaMenuToggle} toggle={toggleMenu} />
 
-      <nav className={megaMenuToggle ? styles.menubarsolid : styles.menubar}>
+      <nav
+        className={megaMenuToggle ? styles.menubarsolid : styles.menubar}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
         <button className={styles.menuButton} onClick={toggleMenu}>
           {megaMenuToggle ? <Minimize2 /> : <Menu />}
         </button>
 
         <Link className={styles.logo} href={"/"}>
-          Alden
+          <Image
+            id="logo"
+            src={megaMenuToggle || isHovering ? "/logo2.svg" : "/logo1.svg"}
+            width={100}
+            height={50}
+            alt="logo"
+          />
         </Link>
 
         <div className={styles.shop}>
