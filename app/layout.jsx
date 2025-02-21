4ce64732f0880793065dc216d2b5128a33e85da8
Virtue Engine <101955083+virtuedevelopment@users.noreply.github.com>
2025-02-21 13:53:34 -0500

    displaying project

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
-        <Locked/>
+        {/* <Locked/> */}
       </body>
     </html>
   );
